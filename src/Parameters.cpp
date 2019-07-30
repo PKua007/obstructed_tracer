3c43608944a2c386d7fa11e8f76a022f96f080d7
Piotr Kubala <pkua.log@gmail.com>
2019-07-30 20:04:47 +0200

    Tracer size in Parameters

--- src/Parameters.cpp
+++ src/Parameters.cpp
@@ -9,6 +9,7 @@
 
 #include "Parameters.h"
 #include "Config.h"
+#include "utils/Assertions.h"
 
 Parameters::Parameters(std::istream& input) {
     auto config = Config::parse(input, '=', true);
@@ -18,15 +19,26 @@ Parameters::Parameters(std::istream& input) {
             this->sigma = config.getFloat(key);
         else if (key == "numberOfSteps")
             this->numberOfSteps = config.getUnsignedLong(key);
+        else if (key == "tracerRadius")
+            this->tracerRadius = config.getFloat(key);
         else if (key == "imageFile")
             this->imageFile = config.getString(key);
         else
             std::cerr << "[Parameters::Parameters] Warning: unknown parameter " << key << std::endl;
     }
+
+    this->validateParameters();
 }
 
 void Parameters::print(std::ostream& out) {
     out << "sigma         : " << this->sigma << std::endl;
     out << "numberOfSteps : " << this->numberOfSteps << std::endl;
+    out << "tracerRadius  : " << this->tracerRadius << std::endl;
     out << "imageFile     : " << this->imageFile << std::endl;
+}
+
+void Parameters::validateParameters() const {
+    Validate(sigma > 0.f);
+    Validate(numberOfSteps > 0);
+    Validate(tracerRadius >= 0.f);
 }
