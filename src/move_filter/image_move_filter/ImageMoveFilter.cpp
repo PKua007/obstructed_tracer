c45e8f8ee3a34d08b27910185fa7d73b539be41a
Piotr Kubala <pkua.log@gmail.com>
2019-08-27 12:12:29 +0200

    ImageMoveFilter prepared for CUDA

--- src/move_filter/image_move_filter/ImageMoveFilter.cpp
+++ src/move_filter/image_move_filter/ImageMoveFilter.cpp
@@ -23,24 +23,36 @@ namespace {
     ImageMove operator-(ImagePoint p1, ImagePoint p2) {
         return {p1.x - p2.x, p1.y - p2.y};
     }
+
+
 }
 
 ImageMoveFilter::ImageMoveFilter(Image image, ImageBoundaryConditions *imageBC, unsigned int seed) :
         width{image.getWidth()}, height{image.getHeight()}, imageBC{imageBC} {
     this->randomGenerator.seed(seed);
-    this->validPointsMap.reserve(image.getNumberOfPixels());
+    this->validPointsMapSize = image.getNumberOfPixels();
+    this->validPointsMap = new bool[this->validPointsMapSize];
+    this->validTracerIndicesCache = new size_t[this->validPointsMapSize];
     this->imageBC->installOnImage(image);
 
     // Image y axis starts from left upper corner downwards, so image is scanned from the bottom left, because
     // validPointsMap is in "normal" coordinate system, with (0, 0) in left bottom corner
-    for (std::size_t y = 0; y < this->height; y++) {
-        for (std::size_t x = 0; x < this->width; x++) {
+    size_t i = 0;
+    for (size_t y = 0; y < this->height; y++) {
+        for (size_t x = 0; x < this->width; x++) {
             if (image(x, this->height - y - 1) == WHITE)
-                this->validPointsMap.push_back(true);
+                this->validPointsMap[i] = true;
             else
-                this->validPointsMap.push_back(false);
+                this->validPointsMap[i] = false;
+
+            i++;
         }
     }
+}
+
+ImageMoveFilter::~ImageMoveFilter() {
+    delete [] this->validPointsMap;
+    delete [] this->validTracerIndicesCache;
 }
 
 void ImageMoveFilter::rebuildValidTracersCache(float radius) {
@@ -50,10 +62,13 @@ void ImageMoveFilter::rebuildValidTracersCache(float radius) {
         return;
 
     this->radiusForTracerCache = radius;
-    this->validTracerIndicesCache.clear();
-    for (std::size_t i = 0; i < this->validPointsMap.size(); i++)
-        if (this->isPointValid(this->indexToPoint(i), radius))
-            this->validTracerIndicesCache.push_back(i);
+    this->validTracerIndicesCacheSize = 0;
+    for (size_t i = 0; i < this->validPointsMapSize; i++) {
+        if (this->isPointValid(this->indexToPoint(i), radius)) {
+            this->validTracerIndicesCache[this->validTracerIndicesCacheSize] = i;
+            this->validTracerIndicesCacheSize++;
+        }
+    }
 }
 
 bool ImageMoveFilter::checkValidPointsMap(ImagePoint point) const {
@@ -85,17 +100,17 @@ bool ImageMoveFilter::isPointValid(ImagePoint point, float pointRadius) const {
 
 bool ImageMoveFilter::isLineValid(ImagePoint from, ImagePoint to, float pointRadius) const {
     ImageMove imageMove = to - from;
-    if (std::abs(imageMove.x) > std::abs(imageMove.y)) {
+    if (abs(imageMove.x) > abs(imageMove.y)) {
         float a = float(imageMove.y) / float(imageMove.x);
         for (int x = from.x; x != to.x; x += sgn(imageMove.x)) {
-            int y = static_cast<int>(std::round(from.y + a * (x - from.x)));
+            int y = static_cast<int>(round(from.y + a * (x - from.x)));
             if (!this->isPointValid({ x, y }, pointRadius))
                 return false;
         }
     } else {
         float a = float(imageMove.x) / float(imageMove.y);
         for (int y = from.y; y != to.y; y += sgn(imageMove.y)) {
-            int x = static_cast<int>(std::round(from.x + a * (y - from.y)));
+            int x = static_cast<int>(round(from.x + a * (y - from.y)));
             if (!this->isPointValid({ x, y }, pointRadius))
                 return false;
         }
@@ -103,8 +118,8 @@ bool ImageMoveFilter::isLineValid(ImagePoint from, ImagePoint to, float pointRad
     return true;
 }
 
-ImagePoint ImageMoveFilter::indexToPoint(std::size_t index) const {
-    Expects(index < this->validPointsMap.size());
+ImagePoint ImageMoveFilter::indexToPoint(size_t index) const {
+    Expects(index < this->validPointsMapSize);
     return {static_cast<int>(index % this->width), static_cast<int>(index / this->width)};
 }
 
@@ -128,13 +143,13 @@ Tracer ImageMoveFilter::randomValidTracer(float radius) {
     Expects(radius >= 0.f);
 
     this->rebuildValidTracersCache(radius);
-    if (this->validTracerIndicesCache.empty())
+    if (this->validTracerIndicesCacheSize == 0)
         throw std::runtime_error("No valid points found in a given image");
 
-    float floatCacheIndex = this->uniformDistribution(this->randomGenerator) * this->validTracerIndicesCache.size();
-    std::size_t cacheIndex = static_cast<std::size_t>(floatCacheIndex);
-    Assert(cacheIndex < this->validTracerIndicesCache.size());
-    std::size_t tracerIndex = this->validTracerIndicesCache[cacheIndex];
+    float floatCacheIndex = this->uniformDistribution(this->randomGenerator) * this->validTracerIndicesCacheSize;
+    size_t cacheIndex = static_cast<size_t>(floatCacheIndex);
+    Assert(cacheIndex < this->validTracerIndicesCacheSize);
+    size_t tracerIndex = this->validTracerIndicesCache[cacheIndex];
     ImagePoint imagePosition = this->indexToPoint(tracerIndex);
 
     float pixelOffsetX = this->uniformDistribution(this->randomGenerator);
@@ -144,11 +159,11 @@ Tracer ImageMoveFilter::randomValidTracer(float radius) {
     return Tracer(tracerPosition, radius);
 }
 
-std::size_t ImageMoveFilter::getNumberOfAllPoints() const {
-    return this->validPointsMap.size();
+size_t ImageMoveFilter::getNumberOfAllPoints() const {
+    return this->validPointsMapSize;
 }
 
-std::size_t ImageMoveFilter::getNumberOfValidTracers(float radius) {
+size_t ImageMoveFilter::getNumberOfValidTracers(float radius) {
     this->rebuildValidTracersCache(radius);
-    return this->validTracerIndicesCache.size();
+    return this->validTracerIndicesCacheSize;
 }
--- src/move_filter/image_move_filter/ImageMoveFilter.h
+++ src/move_filter/image_move_filter/ImageMoveFilter.h
@@ -20,13 +20,17 @@ private:
     std::mt19937 randomGenerator;
     std::uniform_real_distribution<float> uniformDistribution{0.f, 1.f};
 
-    std::size_t width;
-    std::size_t height;
-    ImageBoundaryConditions *imageBC;
-    std::vector<bool> validPointsMap;
+    size_t width{};
+    size_t height{};
+    ImageBoundaryConditions *imageBC{};
+    bool* validPointsMap{};
+    size_t validPointsMapSize{};
 
     float radiusForTracerCache = -1.0;
-    std::vector<std::size_t> validTracerIndicesCache;
+    size_t *validTracerIndicesCache{};
+    size_t validTracerIndicesCacheSize{};
+
+    ImageMoveFilter(const ImageMoveFilter &other) { }
 
     void rebuildValidTracersCache(float radius);
     bool isPointValid(ImagePoint point, float pointRadius) const;
@@ -36,12 +40,13 @@ private:
 
 public:
     ImageMoveFilter(Image image, ImageBoundaryConditions *imageBC, unsigned int seed);
+    ~ImageMoveFilter();
 
     bool isMoveValid(Tracer tracer, Move move) const override;
     Tracer randomValidTracer(float radius) override;
 
-    std::size_t getNumberOfAllPoints() const;
-    std::size_t getNumberOfValidTracers(float radius);
+    size_t getNumberOfAllPoints() const;
+    size_t getNumberOfValidTracers(float radius);
 };
 
 #endif /* IMAGEMOVEFILTER_H_ */
